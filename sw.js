eb822772500256adfc05662ccad2ac46d69df605
seignat-gif <seignat@googlemail.com>
2026-09-17 12:03:14 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,37 @@
-const CACHE='lkw-navi-v05-shell';
-const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+const CACHE='lkw-navi-v20-static';
+
+self.addEventListener('install',e=>{
+
+  self.skipWaiting();
+
+});
+
+self.addEventListener('activate',e=>{
+
+  e.waitUntil(
+
+    caches.keys()
+
+      .then(keys=>Promise.all(keys.map(k=>caches.delete(k))))
+
+      .then(()=>self.clients.claim())
+
+  );
+
+});
+
 self.addEventListener('fetch',e=>{
- if(e.request.method!=='GET') return;
- e.respondWith(fetch(e.request).then(r=>{const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;})
- .catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+
+  if(e.request.method!=='GET') return;
+
+  if(e.request.mode==='navigate'){
+
+    e.respondWith(
+
+      fetch(e.request,{cache:'no-store'})
+
+    );
+
+  }
+
 });
